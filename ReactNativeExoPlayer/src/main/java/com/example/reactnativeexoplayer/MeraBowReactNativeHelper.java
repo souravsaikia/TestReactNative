b78c1c0beda0d0f042295c875d1edc7e0bfe8c21
souravsaikia <sourav.saikia@tokopedia.com>
2023-02-18 23:17:47 +0530

    test

---
+++ ReactNativeExoPlayer/src/main/java/com/example/reactnativeexoplayer/MeraBowReactNativeHelper.java
@@ -0,0 +1,27 @@
+package com.example.reactnativeexoplayer;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class MeraBowReactNativeHelper extends ReactContextBaseJavaModule {
+
+    public MeraBowReactNativeHelper(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "ReactNativeExoPlayer";
+    }
+
+    @ReactMethod
+    public void sayHello() {
+        Toast.makeText(getReactApplicationContext(), "Hello from MyModule!", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/exoplayersample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.exoplayersample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
